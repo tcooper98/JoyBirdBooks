062b4215f79a1859c11b04deb7315444220994a7
tcooper98 <97857190+tcooper98@users.noreply.github.com>
2023-01-23 12:23:13 -0500

    a start

---
+++ src/components/Hero/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+function HeroBanner(){
+    return(
+        <div className="header-container">
+        <h1>Dive Into</h1>
+        <h2>Something New</h2>
+        </div>
+    )
+    
+}
+
+export default HeroBanner;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import HeroBanner from '../../components/Hero/Header'
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+    <>
+    <h1>Home</h1>
+    <HeroBanner>
+
+    </HeroBanner>
+    </>
+    )
 }
