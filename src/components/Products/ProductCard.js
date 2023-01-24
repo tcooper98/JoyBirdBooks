51f289c434550d1137cb1c2696788c26c19b7a9f
tcooper98 <97857190+tcooper98@users.noreply.github.com>
2023-01-24 15:57:28 -0500

    More work on homepage still need to style/format

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,18 +1,60 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { ProductItems } from "./ProductItems.js";
 import "./ProductCard.css";
+
 
 function Product() {
     return (
         <div className="wrapper">
+          <Carousel
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+  
+        
+        keyBoardControl={true}
+        customTransition="all .5"
+       transitionDuration={500}
+       containerClass="carousel-container"
+       removeArrowOnDeviceType={["tablet", "mobile"]}
+  
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+>
           <ProductCard/>
           <ProductCard/>
           <ProductCard/>
           <ProductCard/>
+           </Carousel>
+        
           
         </div>
     )
 }
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 465 },
+    items: 2,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
 
 //Building map for product card so it can be populated with data later on
 function ProductCard() {
@@ -39,5 +81,7 @@ function ProductCard() {
     </>
   )
 }
+
+
 
 export default Product;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,6 +10,7 @@ export default function Home() {
         <HeroBanner>
 
         </HeroBanner>
+        
 
         <div className="fresh-off-press">
 
@@ -17,6 +18,74 @@ export default function Home() {
             
             {/* <BookCard/> */}
             <Product/>
+
+            <div className="spotlight">
+              <img src="images/Poissant.jpg" alt="David James Poissant"/>
+             <div className="spotlight-text">
+              <h1>Local Author Spotlight</h1>
+               <p>DAVID JAMES POISSANT is the author of the story collection The Heaven of Animals, in print in five languages, winner of the GLCA
+                New Writers Award, and a finalist for the Los Angeles Times Book Prize. He teaches the MFA program at the University of Central Florida 
+                and lives in Orlando with his wife and daughters. His debut novel, Lake Life will be published by Simon & Schuster on July 7, 2020.
+               </p>
+               <button>Learn More</button>
+             </div>
+            </div>
+
+            <div className="book-spotlight">
+                <h1>Book Spotlight</h1>
+            </div>
+            
+            <Product/>
+
+            <div className="testimonials">
+                <h1>Testimonials</h1>
+            </div>
+
+            <div className="community">
+              <img src="images/community.jpg" alt="Band playing at community event"/>
+              <h1>COMMUNITY</h1>
+               <p>We are Orlando's very own used bookseller, focused on bringing together local authors and artists to share their work and celebrate 
+                the spirit of art, literacy, and community in a collaborative space. View our calender for events such as book readings, live music,
+                and vendor street festivals.
+               </p>
+               <button>CALENDER</button>
+            </div>
+
+            <div className="newsletter">
+              <h1>SUBSCRIBE TO OUR NEWSLETTER!</h1>
+              <label>
+                        Type your email here:
+                        <input type="text" name="email"></input>
+               </label>
+
+                <button>SEND</button>
+            </div>
+
+            <div className="instgram">
+               <h1>Instgram feed</h1>
+
+            </div>
+
+            <div className="visit">
+                <h1>Visit Us</h1>
+                <h1>Address</h1>
+                <p>Joybird Books LLC 
+                    3018 Corrine Drive
+                    Orlando, FL 32803
+                    407 951-5436
+                </p>
+                <h1>Working Hours</h1>
+                <p>Monday 10AM - 5PM</p>
+                <p>Tuesday 10AM - 5PM</p>
+                <p>Wednesday 10AM - 5PM</p>
+                <p>Thursday 10AM - 5PM</p>
+                <p>Friday 10AM - 5PM</p>
+                <p>Saturday 10AM - 5PM</p>
+                <p>Sunday 10AM - 5PM</p>
+                
+                <button>Get Directions</button>
+
+            </div>
         </div>
     </>
     )
