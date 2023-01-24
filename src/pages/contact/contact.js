1f7fafba39bc9f1cbb0bdced9756dfda67a15962
waleedkhud <77756665+waleedkhud@users.noreply.github.com>
2023-01-24 16:24:09 -0500

    Merge branch 'main'

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -5,35 +5,45 @@ class ContactUs extends Component {
   render() {
     return (
       <div className="form-container">
+
+        <div className="contact-heading">
+          <h2>Contact us</h2>
+        </div>
+        
         <div className="form-row">
-          <div className="form-column">
+          <div className="form-column1">
             <input
               type="text"
               placeholder="First Name"
               className="form-input"
             />
           </div>
-          <div className="form-column">
+          <div className="form-column1">
             <input type="text" placeholder="Last Name" className="form-input" />
           </div>
         </div>
         <div className="form-row">
-          <div className="form-column">
+          <div className="form-column2">
             <input type="email" placeholder="Email" className="form-input" />
           </div>
-          <div className="form-column">
+          <div className="form-column2">
             <input type="text" placeholder="Subject" className="form-input" />
           </div>
         </div>
 
         <div className="form-row">
-          <div className="form-column">
+          <div className="form-column3">
             <textarea
               placeholder="Message"
-              className="form-input form-textarea"
+              className="form-textarea-m"
             ></textarea>
+           
           </div>
+          
         </div>
+        <div className="max">
+          <p>Maximum characters 5000</p>
+          </div>
 
         <div className="form-row">
           <div className="form-column">
