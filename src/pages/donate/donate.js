554c37a7a3cc4e97e1ba9f873036f5d760e668ff
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2023-01-30 11:46:30 -0500

    donate location/hours styling

--- src/pages/donate/donate.js
+++ src/pages/donate/donate.js
@@ -6,63 +6,69 @@ export default function Donate() {
         <div className="donate-container">
             <h1>"Donate & Trade" Hero Banner</h1>
             <form action="" method="" name="donate-form">
-                <h2 className="full-width">What would you like to do today?</h2>
+                <div className="full-width">
+                    <h2>What would you like to do today?</h2>
+                </div>
                 <div>
                     {/*Donate Books Radio Button */}
                     <label for="donate">Donate Books</label>
                     <input type="radio" name="donateTrade" id="yes" value="yes" />
-                    {/*Trade Books Radio Button */}
                 </div>
                 <div>
+                    {/*Trade Books Radio Button */}
                     <label for="trade">Trade Books</label>                      
                     <input type="radio" name="donateTrade" id="no" value="no" />
                 </div>
-                <h3 class="full-width">Contact Information</h3>
-                <div className="form-inputs">
+                <div className="full-width">
+                    <h3 class="sub-title">CONTACT INFORMATION</h3>
+                </div>
+                <div>
                     {/*First Name*/}
-                    <label for="firstName">First Name:*</label>
-                    <input type="text" className="firstName"></input>
+                    <label for="firstName" class="required-input">First Name</label>
+                    <input type="text" placeholder="First Name" className="firstName"></input>
                 </div>
                 <div>
                     {/*Last Name*/}
-                    <label for="lastName">Last Name:*</label>
-                    <input type="text" className="lastName"></input>
+                    <label for="lastName" class="required-input">Last Name</label>
+                    <input type="text" placeholder="Last Name" className="lastName"></input>
                 </div>
                 <div className="full-width">
                     {/*Email*/}
-                    <label for="email">Email:*</label>
-                    <input type="text" className="email"></input>
+                    <label for="email" class="required-input">Email</label>
+                    <input type="text" placeholder="Email Address" className="email"></input>
                 </div>
-                <h3 class="full-width">Book Information</h3>
-                <div className="form-inputs">
+                <div className="full-width">
+                    <h3 class="sub-title">BOOK INFORMATION</h3>
+                </div>
+                <div>
                     {/*Book Title*/}
-                    <label for="bookTitle">Book Title:*</label>
-                    <input type="text" className="bookTitle"></input>
+                    <label for="bookTitle" class="required-input">Book Title</label>
+                    <input type="text" placeholder="Book Title" className="bookTitle"></input>
                 </div>
                 <div>
                     {/*Book Author*/}
-                    <label for="bookAuthor">Book Author:*</label>
-                    <input type="text" className="bookAuthor"></input>
+                    <label for="bookAuthor" class="required-input">Book Author</label>
+                    <input type="text" placeholder="Book Author" className="bookAuthor"></input>
                 </div>
                 <div>
                     {/*Book Edition*/}
-                    <label for="bookEdition">Book Edition/Print:*</label>
-                    <input type="text" className="bookEdition"></input>
+                    <label for="bookEdition" class="required-input">Book Edition/Print</label>
+                    <input type="text" placeholder="Book Edition/Print" className="bookEdition"></input>
                 </div>        
                 <div>
                     {/*Book ISBN*/}
-                    <label for="bookISBN">ISBN/LOC Number:*</label>
-                    <input type="text" className="bookISBN"></input>
+                    <label for="bookISBN" class="required-input">ISBN/LOC Number</label>
+                    <input type="text" placeholder="Book ISBN/LOC Number" className="bookISBN"></input>
                 </div>
                 <div>
                     {/*Book Condition*/}
-                    <label for="bookCondition">Book Condtion:*</label>
-                    <input type="text" className="bookCondition"></input>
+                    <label for="bookCondition" class="required-input">Book Condtion</label>
+                    <input type="text" placeholder="Book Condition" className="bookCondition"></input>
                 </div>
                 <div>
                     {/*Hardcover or Softcover*/}
-                    <label for="bookCover">Hardcover or Softcover:*</label>
-                    <input type="text" className="bookCover"></input>
+                    <label for="bookCover" class="required-input">Hardcover or Softcover</label>
+                    <input type="text" placeholder="Hardcover or Softcover" className="bookCover"></input>
                 </div>
                 <div className="full-width">
                     {/*Submit Button*/}
@@ -81,20 +87,34 @@ export default function Donate() {
                 <h3>What if my books are not all accepted?</h3>
                 <p>Your books we are unable to accept can either be returned to you or given to the appropriate non-profit entity on your behalf.</p>
             </div>
-            <h2>Visit Us</h2>
-            <h3>Address</h3>
-            <p>Joybird Books LLC 3018 Corrine Drive Orlando, Florida 32803 (407) 951-5436</p>
-            <button>Get Directions</button>
-            <h3>Working Hours</h3>
-            <p>
-                Sunday  10am-5pm
-                Monday  10am-5pm
-                Tuesday  10am-5pm
-                Wednesday  10am-5pm
-                Thursday  10am-5pm
-                Friday  10am-5pm
-                Saturday  10am-5pm
-            </p>
         </div>
+            <div className="location">
+                <div className="visit">
+                    <h1>Visit Us</h1>
+                    <img src="images/map.jpg" alt="location of store on map"/>
+                </div>
+                <div className="address">
+                    <h2>Address</h2>
+                    <p>Joybird Books LLC
+                        <br/>
+                        3018 Corrine Drive
+                        <br/>
+                        Orlando, FL 32803
+                        <br/>
+                        407 951-5436
+                    </p>
+                    <button>Get Directions</button>
+                </div>
+                <div className='hours'>
+                    <h2>Working Hours</h2>
+                    <p>Monday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+                    <p>Tuesday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+                    <p>Wednesday &nbsp;&nbsp;10AM - 5PM</p>
+                    <p>Thursday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+                    <p>Friday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+                    <p>Saturday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+                    <p>Sunday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+                </div>     
+            </div>
     </>
 }
