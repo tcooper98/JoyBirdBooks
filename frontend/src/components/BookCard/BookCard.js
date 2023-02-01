9ac378ae9666e51ee81e27c11cea6ff517cf25df
mbdig3480 <email@example.com>
2023-02-01 18:43:52 +0000

    Feb 1, 2023, 1:43 PM

--- frontend/src/components/BookCard/BookCard.js
+++ frontend/src/components/BookCard/BookCard.js
@@ -31,7 +31,7 @@ function Book () {
             {/*rename the styles here to what i created*/}
             {BookItems.map((item, index) => {
                 return (
-                    /*sets className variable. every even/other item gets the end of a row when small. if mod2 is 0, then it's an end*/
+                    /*sets className variable. every even/other item gets the end of a row when small. if mod2 is 0, then it's an end. it dumps out 2 at a time*/
                     <li key={index} className={className}>
                         <div className="row">
                             <div className="p-20">
