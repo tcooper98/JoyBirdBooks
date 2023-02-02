54dbacc48559b68b2bdbdf4f374917d01450ccf5
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2023-02-02 01:18:48 +0000

    Feb 1, 2023, 8:18 PM

--- frontend/src/pages/donate/donate.js
+++ frontend/src/pages/donate/donate.js
@@ -24,12 +24,12 @@ export default function Donate() {
                 <div>
                     {/*Donate Books Radio Button */}
                     <label for="donate" className="donate-radiobuttons">Donate Books</label>
-                    <input type="radio" name="donateTrade" id="yes" value="yes" />
+                    <input type="radio" name="donateTrade" id="accent-color" value="yes" />
                 </div>
                 <div>
                     {/*Trade Books Radio Button */}
                     <label for="trade" className="donate-radiobuttons">Trade Books</label>                      
-                    <input type="radio" name="donateTrade" id="no" value="no" />
+                    <input type="radio" name="donateTrade" id="accent-color" value="no" />
                 </div>
                 <div className="full-width">
                     <h2 class="sub-title">CONTACT INFORMATION</h2>
