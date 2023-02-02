bb651510fcc0b4045a56d3840f71821b7488740c
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2023-02-02 00:39:06 +0000

    Feb 1, 2023, 7:39 PM

--- frontend/src/pages/donate/donate.js
+++ frontend/src/pages/donate/donate.js
@@ -82,7 +82,7 @@ export default function Donate() {
                     <label for="bookCover" class="required-input">Hardcover or Softcover</label>
                     <input type="text" placeholder="Hardcover or Softcover" className="bookCover"></input>
                 </div>
-                <div className="donate-columnb">
+                <div className="full-width">
                     {/*Submit Button*/}
                     <button className="donate-submitbutton" value="Submit">Submit</button>
                 </div>
