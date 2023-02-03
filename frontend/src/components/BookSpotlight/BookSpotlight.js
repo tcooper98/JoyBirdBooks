654563e69280956ae06d47f120d43c7706357857
mbdig3480 <email@example.com>
2023-02-03 01:51:46 +0000

    Feb 2, 2023, 8:51 PM

---
+++ frontend/src/components/BookSpotlight/BookSpotlight.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import './BookSpotlight.css';
+import { Link } from 'react-router-dom';
+
+export default function BookSpotlight() {
+    
