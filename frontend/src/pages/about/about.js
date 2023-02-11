270ed2290759f64efb98815a513601cf0b8f1b7d
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2023-02-11 23:18:56 +0000

    Feb 11, 2023, 6:18 PM

--- frontend/src/pages/about/about.js
+++ frontend/src/pages/about/about.js
@@ -3,19 +3,31 @@ import "./about.css";
 
 function About() {
   return (
-    <div className="about-us-container">
-      <div className="image-container1">
-        <img src={require("./joybirdaboutus.jpg")} alt="About Us" className="img-aboutus"/>
+    <>
+    
+      <div className="about-us-container">
+        <div className="image-container1">
+          <img src={require("./joybirdaboutus.jpg")} alt="About Us" className="img-aboutus"/>
+        </div>
+        <div className="text-container">
+          <h1 className="h1-aboutus">About Us</h1>
+          <p className="p-aboutus">
+            We are Orlando’s very own used bookseller, focused on bringing
+            together local authors and artists to share their work and celebrate
+            the spirit of art, literacy, and community in a collaborative space.
+          </p>
+        </div>
       </div>
-      <div className="text-container">
-        <h1 className="h1-aboutus">About Us</h1>
-        <p className="p-aboutus">
-          We are Orlando’s very own used bookseller, focused on bringing
-          together local authors and artists to share their work and celebrate
-          the spirit of art, literacy, and community in a collaborative space.
-        </p>
+
+      <div className="newsletter">
+        <h1>SUBSCRIBE TO OUR NEWSLETTER!</h1>    
+        <label>              
+          <input type="text" name="email" placeholder="enter your email"></input>
+        </label>
+        <button>SEND</button>
       </div>
-    </div>
+
+    </>
   );
 }
 
