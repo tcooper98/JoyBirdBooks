8ace5378acb75b0e1484aa9e978dd41a2d893432
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2023-02-12 00:24:14 +0000

    Feb 11, 2023, 7:24 PM

--- frontend/src/pages/about/about.js
+++ frontend/src/pages/about/about.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./about.css";
 
 function About() {
+
+  const handleClick = () => {
+        window.location.href = 'https://www.google.com/maps/dir//joybird+books/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88e76fa80d58a57d:0xcb3e5d096693157b?sa=X&ved=2ahUKEwi3sti11vb8AhVdTTABHa5sDYkQ9Rd6BAhWEAU';
+    };
+
   return (
     <>
     
@@ -19,13 +24,42 @@ function About() {
         </div>
       </div>
 
+      <div className="location-donate">
+        <div className="visit-donate">
+          <h1>Visit Us</h1>
+          <img src="images/map.jpg" alt="location of store on map" className="donate-visitimg"/>
+        </div>
+        <div className="address-donate">
+          <h1>Address</h1>
+          <p>Joybird Books LLC
+              <br/>
+              3018 Corrine Drive
+              <br/>
+              Orlando, FL 32803
+              <br/>
+              407 951-5436
+          </p>
+          <button onClick={handleClick}>Get Directions</button>
+        </div>
+        <div className='hours-donate'>
+            <h1>Working Hours</h1>
+            <p>Monday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+            <p>Tuesday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+            <p>Wednesday &nbsp;&nbsp;10AM - 5PM</p>
+            <p>Thursday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+            <p>Friday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+            <p>Saturday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+            <p>Sunday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10AM - 5PM</p>
+        </div>   
+      </div>
+
       <div className="newsletter">
         <h1>SUBSCRIBE TO OUR NEWSLETTER!</h1>    
         <label>              
           <input type="text" name="email" placeholder="enter your email"></input>
         </label>
         <button>SEND</button>
-      </div>
+      </div> 
 
     </>
   );
