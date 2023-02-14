59bbb411c12a52c5b3c145fb27693a6d8b61fafe
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2023-02-14 16:56:55 +0000

    Feb 14, 2023, 11:56 AM

--- frontend/src/pages/newsletter/newsletter.js
+++ frontend/src/pages/newsletter/newsletter.js
@@ -1,4 +1,13 @@
 import React from 'react'
 export default function News() {
-    return <h1>News Letter</h1>
+    return <>
+        <h1>News Letter</h1>
+        <div className="newsletter">
+            <h1>SUBSCRIBE TO OUR NEWSLETTER!</h1>    
+            <label>              
+            <input type="text" name="email" placeholder="Enter Your Email"></input>
+            </label>
+            <button>SEND</button>
+        </div> 
+    </>
 }
