d9362e1e9e2aa5d85e002aa62a730b7d0d2d3a18
waleedkhud <77756665+waleedkhud@users.noreply.github.com>
2023-02-18 00:59:34 -0500

    Update: still can't highlight the events

--- frontend/src/pages/calender/calender.js
+++ frontend/src/pages/calender/calender.js
@@ -8,84 +8,100 @@ import "./calender.css"
 const events = [{
     id: 1,
     color: '#7e9e32',
-    from: '2023-02-05T18:00:00+00:00',
-    to: '2023-02-05T19:00:00+00:00',
+    from: '2023-02-15T18:00:00+00:00',
+    to: '2023-02-15T19:00:00+00:00',
     title: 'Storytime'
 }, {
     id: 2,
     color: '#7e9e32',
-    from: '2023-02-06T13:00:00+00:00',
-    to: '2023-02-06T14:00:00+00:00',
+    from: '2023-02-16T13:00:00+00:00',
+    to: '2023-02-16T14:00:00+00:00',
     title: 'Storytime'
 }, {
     id: 3,
     color: '#7e9e32',
-    from: '2023-02-07T13:00:00+00:00',
-    to: '2023-02-07T20:00:00+00:00',
+    from: '2023-02-20T13:00:00+00:00',
+    to: '2023-02-20T20:00:00+00:00',
     title: 'Zine Meetup'
 },
 {
     id: 4,
     color: '#7e9e32',
-    from: '2023-02-10T13:00:00+00:00',
-    to: '2023-02-10T20:00:00+00:00',
+    from: '2023-02-26T13:00:00+00:00',
+    to: '2023-02-26T20:00:00+00:00',
     title: 'Publishing Workshop'
 },
 {
     id: 5,
     color: '#7e9e32',
-    from: '2023-02-12T13:00:00+00:00',
-    to: '2023-02-12T20:00:00+00:00',
+    from: '2023-02-28T13:00:00+00:00',
+    to: '2023-022-28T20:00:00+00:00',
     title: 'Margaret Edwards Author Spotlight'
 },
 {
     id: 6,
     color: '#7e9e32',
-    from: '2023-02-14T13:00:00+00:00',
-    to: '2023-02-14T20:00:00+00:00',
+    from: '2023-03-01T13:00:00+00:00',
+    to: '2023-03-01T20:00:00+00:00',
     title: 'Little Weird Book Club'
 },
 {
     id: 6,
     color: '#7e9e32',
-    from: '2023-02-15T13:00:00+00:00',
-    to: '2023-02-15T20:00:00+00:00',
+    from: '2023-03-15T13:00:00+00:00',
+    to: '2023-03-15T20:00:00+00:00',
     title: 'Open Mic & Poetry Slam'
 }];
 
-export default function Calender() {
-    const [value, onChange] = useState(new Date());
+function isSameDay(d1, d2) {
+    return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
+}
 
-    function isSameDay(d1, d2) {
-        return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
-    }
+function MyCalendar() {
+    const [value, setValue] = useState(new Date());
 
     function handleDateClick(date) {
-        events.forEach(event => {
-            if (isSameDay(date, new Date(event.from))) {
-                highlightCell(date, event.color);
-            }
+        const eventsOnDay = events.filter(event => isSameDay(date, new Date(event.from)));
+        eventsOnDay.forEach(event => {
+            highlightCell(date, "#7e9e32");
         });
     }
 
     function highlightCell(date, color) {
         const cell = document.querySelector(`[aria-label="${date.toDateString()}"]`);
-        cell.style.backgroundColor = color;
+        if (cell) {
+            cell.style.backgroundColor = color;
+        }
     }
-    
+
+    const upcomingEvents = events
+        .filter(event => new Date(event.from) >= value)
+        .sort((a, b) => new Date(a.from) - new Date(b.from))
+        .slice(0, 3);
 
     return (
-        <div className='calender-container'>
-        <div className="calender">
-            <Calendar onChange={onChange} value={value} onClickDay={handleDateClick} />
-               
-        </div>
-        <div className='events'>
-
-
-        </div>
-        </div>
+        <div className='calendar-container'>
+            <div className="calendar">
+                <Calendar onChange={setValue} value={value} onClickDay={handleDateClick} />
+            </div>
+            <div className='events'>
+                <div className='event-title'>
+                    <h2>Upcoming Events</h2>
+                </div>
+                <div className='event-list'>
+                <ul>
+                    {upcomingEvents.map(event => (
+                        <li key={event.id}>
+                            {`${event.title} @ ${new Date(event.from).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} ${new Date(event.from).toLocaleDateString()}`}
+                        </li>
+                    ))}
+                </ul>
+                
+                </div>
+                </div>
+            </div>
+        
     );
 }
 
-
+export default MyCalendar;
