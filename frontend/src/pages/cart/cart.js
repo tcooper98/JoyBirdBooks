bd39ad8397836d0d046c6710762fb421ce95ac9c
tcooper98 <97857190+tcooper98@users.noreply.github.com>
2023-03-02 18:02:15 -0500

    save

--- frontend/src/pages/cart/cart.js
+++ frontend/src/pages/cart/cart.js
@@ -6,7 +6,19 @@ import { Rating } from '@mui/lab'
 
 const Cart = () => {
     return( 
-         <p>hi</p>
+         <div className='productcontainer'>
+        {/* {loading ? 
+        (<p>loading</p>) 
+        : error ? 
+        (<p>error</p>) 
+        : cart.length <= 0 ? 
+        (<p>Cart is empty</p>) 
+        : (<p>Display</p>)
+        
+        }
+         */}
+
+      </div>
     )
 }
 
