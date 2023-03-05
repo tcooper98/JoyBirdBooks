22247f85bdcf2807810fc05d4e85f42888e8dae0
tcooper98 <97857190+tcooper98@users.noreply.github.com>
2023-03-05 17:51:40 -0500

    loading circle

--- frontend/src/pages/products/products.js
+++ frontend/src/pages/products/products.js
@@ -6,7 +6,7 @@ import Rating from '@mui/material/Rating';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../redux/actions/productActions'
 import { useEffect } from 'react';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 
 
@@ -35,52 +35,6 @@ export default function Products() {
 }
 
 
-// // waiting to figure out where to host database
-// export function ProductCard() {
- 
-  
-//    //fetch products from strapi
-//    const[results] = useQuery({ query: ITEM_QUERY });
-     
-//    const { data, fetching, error } = results;
-
-//    if (fetching) return <p>Loading...</p>;
-//       if (error) return <p>Oh no... {error.message}</p>;
-
-//       const items = data.items.data;
-//       console.log(items);
-   
-//     //formatting how products are displayed 
-//     return (
-//       <div className='productcontainer'>
-//          {items.map((item) => (
-         
-//           <div key={item.attributes.slug} className="product-card">
-          
-//            <div className="product_container" >
-//             {/* linking to individual product page */}
-          
-//            <Link to={`/product/${item.attributes.slug}`} style={{ textDecoration: 'none', color: 'inherit'}}> 
-//           <div className="product-body">
-//           <img className="product-image" src={item.attributes.image.data.attributes.formats.large.url} alt={item.attributes.name}/>
-//           <h1 className="product-title">{item.attributes.name}</h1>
-//           <Rating name="size-small" defaultValue={item.attributes.rating} size="small"/>
-//           <h3 className="product-author">By {item.attributes.author}</h3>
-//          <p className="product-price">${item.attributes.price}</p>
-        
-        
-//         </div>
-//          </Link>
-         
-//       </div> 
-//       </div>
-//          ))}
-
-//       </div>
-//     )
-  
-//   }
-
 function ProductCard() {
  const dispatch = useDispatch();
 
@@ -96,7 +50,7 @@ function ProductCard() {
  
     return (
       <div className='productcontainer'>
-        {loading ? (<p>loading</p>) 
+        {loading ? (<CircularProgress/>) 
         : error ? (<p>error</p>) 
         : (products.map((product) => (
           <div key={product._id} className="product-card">
--- frontend/src/pages/products/soloProductCard.js
+++ frontend/src/pages/products/soloProductCard.js
@@ -9,7 +9,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addCartItem } from '../../redux/actions/cartActions';
 import React from 'react';
 import { Link as ReactLink} from "react-router-dom";
-
+import { CircularProgress, Skeleton } from '@mui/material';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 
 const SoloProductCard = () => {
@@ -62,32 +63,32 @@ const addBuyNow = (id) => {
   }, [id]);
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <CircularProgress/>
   }
 
-  if(isLoading) {
-    return <p>Loading...</p>
-  }
     
     //formatting how products are displayed 
     return (
-       
+      
       <div className='soloproduct'>
-         
+      <Link to={`/products`} style={{ textDecoration: 'none', color: 'inherit'}}> 
+      <KeyboardArrowLeftIcon fontSize="large"/>
+      </Link>
           
         
           <div key={product._id} className="product-details">
           <img src={product.image} alt={product.name}/>
           <div className="product-info">
           <h1>{product.name}</h1>
-          <h3>By {product.author}</h3>
+          <h3 >By {product.author}</h3>
           <Rating name="size-small" value={product.rating} size="small" readOnly/>
-         <p>{product.description}</p>
-         <h2>${product.price}</h2>
-          <input className="ship" type="radio" value=""></input>
+           <h2>${product.price}</h2>
+         <p className="product-details">{product.description}</p>
+        
+          {/* <input className="ship" type="radio" value=""></input>
                         <label for="ship">Ship This Item - Qualifies for Free Shipping</label> <br/> <br/>
                         <input className="buy" type="radio" value=""></input>   
-                        <label for="buy">Pick up at store - 3018 Corrine Dr, Orlando, FL 32803</label><br/>
+                        <label for="buy">Pick up at store - 3018 Corrine Dr, Orlando, FL 32803</label><br/> */}
               
                 <button className='leftbutton' isDisabled={product.stock <= 0} onClick={()=>addItem(product._id)}>Add to Cart</button>
                  <Link to={`/cart`} style={{ textDecoration: 'none', color: 'inherit'}}> 
@@ -111,13 +112,22 @@ const addBuyNow = (id) => {
                 <p>Share your thoughts with the community</p>
                 <button className='product-review'>Write a product review</button>
              </div>
-
-            {product.reviews.map((review) => (
+            {product.reviews.length ===0 ? (
+              <div className='noReviews'>
+              <p className='reviewResult'>There Are No Reviews</p>
+              <Box sx={{ width: 500 }}>
+              <Skeleton/>
+              <Skeleton sx={{ height: 200 }}/>
+              </Box>
+            
+              </div>
+            ):(product.reviews.map((review) => (
              <div className="review-info">
-                  <select name="rating" id="rating" className='ratingdropdown' >
+                  {/* <select name="rating" id="rating" className='ratingdropdown' >
                   <option value="high">Sort by Highest Rating</option>
                   <option value="low">Sort by Lowest Rating</option>
-                 </select>
+                 </select> */}
+                 <p className='reviewResult'>Showing All Reviews</p>
               
               <div className='review-content'>
                <h2>{review.title}</h2>
@@ -128,7 +138,8 @@ const addBuyNow = (id) => {
                 
               </div>
               </div>
-               ))}
+               ))
+              )}
 
         </div>
                 
